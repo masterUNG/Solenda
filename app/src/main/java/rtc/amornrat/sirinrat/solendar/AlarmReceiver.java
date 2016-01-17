1999b23a9ccad377cbfcffa3d1e969bc2c49eb3f
masterUNG <phrombutr@gmail.com>
2016-01-17 23:36:39 +0700

    Notification Success

--- app/src/main/java/rtc/amornrat/sirinrat/solendar/AlarmReceiver.java
+++ app/src/main/java/rtc/amornrat/sirinrat/solendar/AlarmReceiver.java
@@ -1,9 +1,13 @@
 package rtc.amornrat.sirinrat.solendar;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.widget.Toast;
 
 /**
@@ -17,7 +21,25 @@ public class AlarmReceiver extends BroadcastReceiver{
         MediaPlayer alarmMediaPlayer = MediaPlayer.create(context, R.raw.intro_start_horse);
         alarmMediaPlayer.start();
 
+//        MainActivity objMainActivity = new MainActivity();
+//        objMainActivity.masterNotification();
+
+        Intent objIntent = new Intent(Intent.ACTION_VIEW);
+        objIntent.setData(Uri.parse("http://androidthai.in.th/about-me.html"));
+        PendingIntent objPendingIntent = PendingIntent.getActivity(context, 0, objIntent, 0);
+
+        Notification objNotification = new Notification.Builder(context)
+                .setSmallIcon(R.drawable.icon_cow)
+                .setContentTitle("Title")
+                .setContentText("Message")
+                .setAutoCancel(true)
+                .setContentIntent(objPendingIntent)
+                .build();
+
+        NotificationManager objNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        objNotificationManager.notify(1000, objNotification);
 
 
-    }
+
+    }   //AlarmReceiver
 }   // Main Class
